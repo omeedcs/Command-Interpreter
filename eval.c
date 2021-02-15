d10bc50427e80ef385e03b1ecca856cc20865a7d
Omeed Tehrani <omeed@cs.utexas.edu>
2021-02-14 18:26:33 -0600

    eval fix

--- eval.c
+++ eval.c
@@ -104,7 +104,8 @@ static void eval_node(node_t *(nptr)) {
         if (nptr->tok == TOK_QUESTION) {
             if (nptr->children[0]->type == INT_TYPE || nptr->children[0]->type == STRING_TYPE || (nptr->children[1]->type == INT_TYPE && nptr->children[2]->type == BOOL_TYPE)) {
                 handle_error(ERR_TYPE);
-        } else {
+            } else {
+                eval_node(nptr->children[0]);
                 if (nptr->children[0]->val.bval == 1) {
                     eval_node(nptr->children[1]);
                     nptr->val.ival = nptr->children[1]->val.ival;
@@ -135,6 +136,7 @@ static void eval_node(node_t *(nptr)) {
             }
         }
 
+ 
         if (nptr->tok == TOK_PLUS) {
             if (nptr->type == INT_TYPE) {
                 nptr->val.ival = nptr->children[0]->val.ival + nptr->children[1]->val.ival;
